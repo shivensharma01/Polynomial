76264ce9fb6d7197417dae34020924e03a108203
Shiven Sharma <shivensharma01@gmail.com>
2024-01-28 20:55:16 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -34,6 +43,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -41,6 +53,9 @@ class Div:
     
     def __repr__(self):
         return "(" + repr(self.p1) + " / " + repr(self.p2) + ")"
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -50,9 +65,12 @@ class Sub:
     def __repr__(self):
         return "(" + repr(self.p1) + " - " + repr(self.p2) + ")"
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+
 # Testing the polynomial expression system
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))  # Output: 5
 
 poly_with_div_and_sub = Sub(Add(Int(10), X()), Div(Mul(Int(4), X()), Int(2)))
-print(poly_with_div_and_sub)
+print(poly_with_div_and_sub.evaluate(2))  # Output: 9.0
